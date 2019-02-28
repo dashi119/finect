c13d0f998d1c6810108cd5196a9a68891b5fe4fa
Masayuki <dashi119@gmail.com>
2019-02-28 08:42:25 +0000

    pain.002sample追加

--- finect/VirtualBank/BankStub.py
+++ finect/VirtualBank/BankStub.py
@@ -1,17 +1,9 @@
-xml_path = "pain001sam.xml"
-# f = open(xml_path)
-
 import xml.etree.ElementTree as ET
-
-tree = ET.parse(xml_path)
-ET.register_namespace('', 'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03')
-root = tree.getroot()
-ns = "{urn:iso:std:iso:20022:tech:xsd:pain.001.001.03}"
-# for child in root:
-#     print(child.tag, child.attrib)
-# ADDRESS_valueタグ配下のvalueタグをすべて選択します
-#Ref_Xpath = "." + ns + "CstmrCdtTrfInitn/" + ns + "PmtInf/" + ns + "CdtTrfTxInf/" + ns + "RmtInf/" + ns + "Strd/" + ns + "Invcee/" + ns + "CtctDtls/" + ns + "Othr"
-Ref_Xpath = ".hoge:CstmrCdtTrfInitn/hoge:PmtInf/hoge:CdtTrfTxInf/hoge:Strd/hoge:Invcee/hoge:CtctDtls/hoge:Othr"
-#print(Ref_Xpath)
-for i in root.findall(Ref_Xpath, namespaces={"hoge" : "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03"}):
+xml_path = "pain001sam.xml"             # pain.001 XML File path on local downloaded from SFTP Server SEND dir
+tree = ET.parse(xml_path)               # Parse pain.001 XML File
+ET.register_namespace('', 'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03') # for omit XML Name Space Prefix from Output XML file(pain.002)
+root = tree.getroot()                   # XML root set
+ns = {'1': 'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03'} # Define dict as ISO20022 XML Default NameSpace This is etree.ElementTree specific methodlogy for default namespace 
+Ref_Xpath = ".//1:CstmrCdtTrfInitn/1:PmtInf/1:CdtTrfTxInf/1:RmtInf/1:Strd/1:Invcee/1:CtctDtls/1:Othr" # Search Xpath for pain.001-pain.002 matching reference tag 
+for i in root.findall(Ref_Xpath, ns):
     print(i.tag, i.text)
